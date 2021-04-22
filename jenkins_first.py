122a87262eb660c85e6e24cd98051c4b840b6194
Fatemeh <f.zendehrouh@gmail.com>
2021-04-22 14:29:15 +0200

    a class which prints out the credenstials

--- jenkins_first.py
+++ jenkins_first.py
@@ -1,6 +1,15 @@
-import pandas as pd
 
-df = pd.DataFrame(columns = ['col1', 'col2'])
+import json
 
-print(df.columns)
-print(df.shape)
+class ClassName():
+    """make a class with two input ."""
+
+    def __init__(self, user, cred):
+        #super(, self).__init__()
+        self.user = user
+        self.cred = cred
+
+    def to_jsonFile()
+        f = {"cred" : cred, "user" : user}
+        json_file = json.dumps(f)
+        print(json_file)
